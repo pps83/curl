4a54f2c46ff545ec69f5cc538ac6fc2e5e871ae9
Pavel P <pavlov.pavel@gmail.com>
2025-04-16 17:44:09 +0200

    Add config-msys.h for libcurl 8.12.1

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -128,7 +128,11 @@
 
 #include "curl_config.h"
 
-#else /* HAVE_CONFIG_H */
+#else /* !HAVE_CONFIG_H */
+
+#ifdef __MSYS__
+#  include "config-msys.h"
+#endif
 
 #ifdef _WIN32
 #  include "config-win32.h"
